e7eb286b6c07e71ee52f4cc015c6d50923dcbe6a
Joao <sousajoao07@hotmail.com>
2020-05-28 23:20:29 +0100

    tomaaa

--- public/js/app.js
+++ public/js/app.js
@@ -19335,8 +19335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! F:\laragon\www\boot\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! F:\laragon\www\boot\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\BleProjFinal\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\BleProjFinal\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-//Route::post('/reads', 'ReadController@store');
+Route::post('/reads', 'ReadController@store');
 Route::put('/reads', 'ReadController@update');
 Route::get('/reads/{id}', 'ReadController@show');
 
